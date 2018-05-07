919a7ff03acb02db0ea17a8e48991a49f06311f9
jia <jia@jiadeMacBook-Pro.local>
2018-05-07 10:07:55 +0800

    according to the loginState display customer information

--- app/pages/myCustomer.js
+++ app/pages/myCustomer.js
@@ -7,7 +7,8 @@ import {
     Text,
     View,
     TouchableOpacity,
-    FlatList
+    FlatList,
+    Alert
   } from 'react-native';
 
 
@@ -15,59 +16,54 @@ import {
     static navigationOptions={
         headerLeft:null
        }
+       componentWillMount(){
+        console.log('登录状态',global.user.loginState)
+          
+       }
       render(){
+          let customers=global.user.loginState?
+          <FlatList
+          data={[
+            {
+                key: '1',
+                customName:'xiaohang',
+                customTeam:'信息部',
+                customSex:'女',
+                customPhone:'19082378236',
+                customPhoneNumber:'127892132937',
+                customSettime:'2018-12-12'
+            }, 
+            {  
+            key: '2',
+            customName:'戴旭',
+            customTeam:'广告部',
+            customSex:'男',
+            customPhone:'1232382378236',
+            customPhoneNumber:'99927892132937',
+            customSettime:'2012-12-12'
+        }
+        ]}
+          renderItem={({item})=><CustomItem 
+          customName={item.customName} 
+          customTeam={item.customTeam} 
+          customSex={item.customSex} 
+          customPhone={item.customPhone} 
+          customPhoneNumber={item.customPhoneNumber}
+          customSettime={item.customSettime}
+          {...this.props}
+          />}
+        />
+          :
+          <View style={bus.noticebox}>
+            <Text>要查看相关信息,请先</Text>
+            <Text style={{color:'red'}} onPress={()=>this.props.navigation.navigate('Login')}>登录</Text>
+          </View>
+          ;
       return(
           <View>
               <View style={bus.busBox}>
-
-                <FlatList
-                  data={[
-                    {
-                        key: '1',
-                        customName:'xiaohang',
-                        customTeam:'信息部',
-                        customSex:'女',
-                        customPhone:'19082378236',
-                        customPhoneNumber:'127892132937',
-                        customSettime:'2018-12-12'
-                    }, 
-                    {  
-                    key: '2',
-                    customName:'戴旭',
-                    customTeam:'广告部',
-                    customSex:'男',
-                    customPhone:'1232382378236',
-                    customPhoneNumber:'99927892132937',
-                    customSettime:'2012-12-12'
-                }
-                ]}
-                  renderItem={({item})=><CustomItem 
-                  customName={item.customName} 
-                  customTeam={item.customTeam} 
-                  customSex={item.customSex} 
-                  customPhone={item.customPhone} 
-                  customPhoneNumber={item.customPhoneNumber}
-                  customSettime={item.customSettime}
-                  {...this.props}
-                  />}
-                />
-
-                <View style={bus.busItem}>
-                <View style={bus.title}>
-                    <View style={{flexDirection:'row',}}><Text style={{color:'#ababab'}}>客户名称</Text><Text style={{color:'#000',marginLeft:matchsize(15)}}>蔡晴</Text></View>
-                    <View style={{flexDirection:'row',}}><Text style={{color:'#ababab'}}>任务名称</Text><Text style={{color:'#000',marginLeft:matchsize(15)}}>投资金单申请</Text></View>
-                  </View>
-                  <View style={bus.content}>
-                    <View style={{flexDirection:'row',alignItems:'center',width:'50%'}}><Text style={{color:'#ababab',fontSize:matchsize(27)}}>性别</Text><Text style={{color:'#99cffe',fontSize:matchsize(27),marginLeft:matchsize(8)}}>女</Text></View>
-                    <View style={{flexDirection:'row',alignItems:'center',width:'50%',justifyContent:'flex-end'}}><Text  style={{color:'#ababab',fontSize:matchsize(27)}}>手机号</Text><Text style={{fontSize:matchsize(27),marginLeft:matchsize(8)}}>15743615276</Text></View>
-                    <View style={{flexDirection:'row',alignItems:'center',marginTop:matchsize(20)}}><Text  style={{color:'#ababab',fontSize:matchsize(27)}}>证件号码</Text><Text style={{fontSize:matchsize(27),marginLeft:matchsize(8)}}>092fhe29873</Text></View>
-                  </View>
-                  <View style={bus.bottom}>
-                      <Text style={{color:'#ababab',fontSize:matchsize(27)}}>创建时间</Text>
-                      <Text style={{color:'#656565',paddingLeft:matchsize(8),fontSize:matchsize(27)}}>2018-04-03</Text>
-                  </View>
-                </View>
-
+                <Text>{global.user.loginState}</Text>
+                {customers}
               </View>
           </View>
       )
@@ -99,6 +95,11 @@ import {
         flexDirection:'row',
         justifyContent:'flex-end',
         alignItems:'center'
+    },
+    noticebox:{
+      flexDirection:'row',
+       width:'100%',
+     justifyContent:'center'
     }
 
   })
