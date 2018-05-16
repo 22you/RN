a4c60c3680c3ea4b0fd80db4ef830f4731a9ea78
jia <jia@jiadeMacBook-Pro.local>
2018-05-16 12:02:10 +0800

    我的客户修改

--- app/pages/customer/customItem.js
+++ app/pages/customer/customItem.js
@@ -6,34 +6,30 @@ export default class CustomItem extends Component{
  render(){
    let swipeoutBtns = [
         {
-          text: '编辑'
+          text: '编辑',
+          type:'secondary'
         },
-        {
-            text: '查看'
-          },
+       
           {
-            text: '删除'
+            text: '删除',
+            type:'delete'
           }
 
       ]
      return(
-        <Swipeout right={swipeoutBtns} style={{marginTop:matchsize(30)}}>
+        <Swipeout right={swipeoutBtns}  style={{marginTop:matchsize(30)}} autoClose>
         <TouchableOpacity style={bus.busItem} onPress={()=>this.props.navigation.navigate('Adduser',{
              ...this.props
         })}>
-        <View style={bus.title}>
-            <View style={{flexDirection:'row',}}><Text style={{color:'#ababab'}}>客户名称</Text><Text style={{color:'#000',marginLeft:matchsize(15)}}>{this.props.customName}</Text></View>
-            <View style={{flexDirection:'row',}}><Text style={{color:'#ababab'}}>部门名称</Text><Text style={{color:'#000',marginLeft:matchsize(15)}}>{this.props.customTeam}</Text></View>
-          </View>
+         
           <View style={bus.content}>
-            <View style={{flexDirection:'row',alignItems:'center',width:'50%'}}><Text style={{color:'#ababab',fontSize:matchsize(27)}}>性别</Text><Text style={{color:'#99cffe',fontSize:matchsize(27),marginLeft:matchsize(8)}}>{this.props.customSex}</Text></View>
-            <View style={{flexDirection:'row',alignItems:'center',width:'50%',justifyContent:'flex-end'}}><Text  style={{color:'#ababab',fontSize:matchsize(27)}}>手机号</Text><Text style={{fontSize:matchsize(27),marginLeft:matchsize(8)}}>{this.props.customPhone}</Text></View>
-            <View style={{flexDirection:'row',alignItems:'center',marginTop:matchsize(20)}}><Text  style={{color:'#ababab',fontSize:matchsize(27)}}>证件号码</Text><Text style={{fontSize:matchsize(27),marginLeft:matchsize(8)}}>{this.props.customCardNumber}</Text></View>
+            <View style={{flexDirection:'row',felx:1,justifyContent:'space-between'}}>
+                <Text style={{color:'#99cffe',fontSize:matchsize(27),marginLeft:matchsize(8)}}><Text style={{color:'#ababab'}}>部门名称:</Text>{this.props.customName}</Text>
+                <Text style={{marginLeft:matchsize(8)}}><Text style={{color:'#ababab'}}>所属部门：</Text>{this.props.customTeam}</Text>
+            </View>
+            <View style={{flexDirection:'row',justifyContent:'flex-end',marginTop:10}}><Text style={{fontSize:matchsize(25),color:'#ababab'}}>{this.props.customSettime}</Text></View>
           </View>
-          <View style={bus.bottom}>
-              <Text style={{color:'#ababab',fontSize:matchsize(27)}}>创建时间</Text>
-              <Text style={{color:'#656565',paddingLeft:matchsize(8),fontSize:matchsize(27)}}>{this.props.customSettime}</Text>
-          </View>
+       
         </TouchableOpacity>
         </Swipeout>
      )
@@ -44,25 +40,10 @@ const bus=StyleSheet.create({
         backgroundColor:'#fff',
         paddingHorizontal:'3%',
     },
- 
-    title:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        borderBottomWidth:1,
-        borderBottomColor:'#ddd',
-        paddingVertical:matchsize(30),
-    },
     content:{
-        borderBottomWidth:1,
-        borderBottomColor:'#ddd',
-        paddingVertical:matchsize(30),
-        flexDirection:'row',
-        flexWrap:'wrap'
-    },
-    bottom:{
-        paddingVertical:matchsize(20), 
-        flexDirection:'row',
-        justifyContent:'flex-end',
-        alignItems:'center'
+        paddingVertical:matchsize(15),
+        flexWrap:'wrap',
+       justifyContent:'space-between'
     }
+  
   })
