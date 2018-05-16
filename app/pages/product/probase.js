a14b70ae154fea45dcbdc5180ec256c6bd3b5b96
jia <jia@jiadeMacBook-Pro.local>
2018-05-16 18:04:28 +0800

    产品基本信息修改

--- app/pages/product/probase.js
+++ app/pages/product/probase.js
@@ -36,12 +36,13 @@ import {
         Alert.alert('请输入投资金额');
         return false;
       }
-   let startUrl=config.api.start+'mmplanId='+this.props.navigation.state.params.item.mmplanId+'&businessFlow=SingleInApprovalFlow'+
+   let startUrl=config.api.start+'userIds='+global.user.userData.userId+'&mmplanId='+this.props.navigation.state.params.item.mmplanId+'&businessFlow=SingleInApprovalFlow'+
                 '&plManageMoneyPlanBuyinfo.buyMoney='+this.state.InvestAmount+'&csInvestmentperson.investId='+
                 this.state.valueCustom+'&csInvestmentperson.investName='+this.state.valueCustomtext;//购买流程启动
                 console.log(startUrl);
     
-      // this.props.navigation.navigate('Apply')
+     
+               this.props.navigation.navigate('Apply')
     }
     componentDidMount(){
      //console.log(this.props.navigation.state.params.item);
