555402c25f614c8669a58a9b36649e02735e82fa
jia <jia@jiadeMacBook-Pro.local>
2018-05-30 13:52:14 +0800

    修改首页的产品购买

--- app/pages/home.js
+++ app/pages/home.js
@@ -18,6 +18,8 @@ import Swiper from 'react-native-swiper'
 import styles from '../styles/style-app'
 import Header from '../components/header'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import config from '../common/config';
+import axios from 'axios'
 export default class Home extends Component {
   static navigationOptions = {
     header:Header,
@@ -31,7 +33,13 @@ export default class Home extends Component {
      index:0,
      banners:[
       require('../images/index/banner.jpg')
-     ]
+     ],
+     mmName:'',
+     yeaRate:'',
+     useCompany:'',
+     investlimit:'',
+     item:[]
+
     };
 }
 setIndex=(index)=>{
@@ -40,7 +48,27 @@ setIndex=(index)=>{
   })
 }
 componentDidMount(){
-  
+ //首页中的产品查询
+ 
+ let proUrl=config.api.prolist+'?start=0&limit=1';
+ axios.get(proUrl)
+ .then((res)=>{
+   if(res.data.success){
+     this.setState({
+      mmName:res.data.data[0].mmName,
+      yeaRate:res.data.data[0].yeaRate,
+      useCompany:res.data.data[0].useCompany,
+      investlimit:res.data.data[0].investlimit,
+      item:res.data.data[0]
+
+     })
+    //console.log(res.data.data[0]);
+   }
+ })
+ .catch((error)=>{
+console.log(error);
+
+ })
 }
 
 addUserMethod=()=>{
@@ -52,6 +80,20 @@ addUserMethod=()=>{
       ...this.props
       
   })
+  }
+}
+
+_goProBase=(item)=>{
+  if(global.user.loginState){
+     // console.log(this.props.navigation)
+      this.props.navigation.navigate('Probase',{item:item})
+  }else{
+     Alert.alert('温馨提示','要购买产品请先登录',
+      [
+          {text:"我知道了", onPress:this.confirm},
+          {text:"去登录", onPress:()=>this.props.navigation.navigate('Login')}
+  ]
+    );
   }
 }
 
@@ -69,6 +111,8 @@ addUserMethod=()=>{
     <View style={{justifyContent:'center',alignItems:'center',flex:1}}>
       <Text style={{flexDirection:'row',justifyContent:'center'}}>loading...</Text>
     </View>;
+    let {mmName,yeaRate,useCompany,investlimit}=this.state;
+   // console.log();
     
     return (
     <View style={[styles.indexBox,{flex: 1}]}>
@@ -107,11 +151,11 @@ addUserMethod=()=>{
          <View style={styles.indexBuy}>
           <View style={styles.indexBuyTitle}>
            <Text style={{fontSize:matchsize(28)}}>20180403001</Text>
-           <Text style={styles.buyTitleTag}>随意宝</Text>
+           <Text style={styles.buyTitleTag}>{mmName}</Text>
           </View>
           <View style={styles.indexBuyRate}>
             <Text style={styles.indexBuyRateHea}>年化益率</Text>
-            <Text  style={styles.indexBuyRatesubHea}>5.65%</Text>
+            <Text  style={styles.indexBuyRatesubHea}>{`${yeaRate}%`}</Text>
           </View>
           <View style={styles.indexBuyInfo}>
            <View style={styles.indexBuyInfoItem}>
@@ -119,16 +163,16 @@ addUserMethod=()=>{
              <Text style={{color:'#acacac',marginTop:10}}>产品类型</Text>
            </View>
            <View style={styles.indexBuyInfoItem}>
-            <Text  style={styles.BuyInfoDt}>12期</Text>
+            <Text  style={styles.BuyInfoDt}>{`${investlimit}期`}</Text>
              <Text  style={{color:'#acacac',marginTop:10}}>投资期限</Text>
            </View>
            <View style={[styles.indexBuyInfoItem,{borderRightWidth:0}]}>
-            <Text style={styles.BuyInfoDt}>个人</Text>
+            <Text style={styles.BuyInfoDt}>{useCompany}</Text>
              <Text  style={{color:'#acacac',marginTop:10}}>授权范围</Text>
            </View>
           </View>
          <View style={{paddingVertical:matchsize(40),marginHorizontal:'3%'}}>
-          <Button  title="立即购买" type="primary"  style={{height:matchsize(70)}} onPress={()=>{}} />
+          <Button  title="立即购买" type="primary"  style={{height:matchsize(70)}} onPress={()=>{this._goProBase(this.state.item)}} />
          </View>  
          </View>
         <View style={{flex:3,backgroundColor:'steelblue',alignItems:'center'}}>
